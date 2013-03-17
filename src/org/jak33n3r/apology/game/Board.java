6344c07249e1fe352ee15a003945ee1fd2c65951
jafelker <joshalan1993@aol.com>
2013-03-17 02:59:37 -0400

    Created Board, Piece, and Tile classes.

---
+++ src/org/jak33n3r/apology/game/Board.java
@@ -0,0 +1,11 @@
+package org.jak33n3r.apology.game;
+
+import java.util.ArrayList;
+
+public class Board {
+	ArrayList<Tile> outer_tiles;
+
+	public Board() {
+
+	}
+}
---
+++ src/org/jak33n3r/apology/game/Tile.java
@@ -0,0 +1,5 @@
+package org.jak33n3r.apology.game;
+
+public class Tile {
+
+}
